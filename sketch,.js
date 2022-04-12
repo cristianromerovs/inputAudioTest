d96fb4e99b187adcfde0a45b60921d0e8986b146
cristianromerovs <cristianromerovs@gmail.com>
2022-04-12 00:30:06 -0400

    fhd camera mic input

--- sketch,.js
+++ sketch,.js
@@ -10,7 +10,7 @@ function setup() {
 function gotSources(deviceList) {
     if (deviceList.length > 0) {
         //set the source to the first item in the deviceList array
-        audioIn.setSource(0);
+        audioIn.setSource(3);
         let currentSource = deviceList[audioIn.currentSource];
         text('set source to: ' + currentSource.deviceId, 5, 20, width);
     }
@@ -20,5 +20,5 @@ function draw() {
     background(0);
     let vol = audioIn.getLevel();
     ellipse(100, 100, vol * 200, vol * 200);
-    // console.log(vol);
+    console.log(vol);
 }
