5d4fadd6f2f1cfe84f2c18a7524378710c59bcbe
cristianromerovs <cristianromerovs@gmail.com>
2022-04-12 00:33:07 -0400

    mic input

--- sketch,.js
+++ sketch,.js
@@ -1,24 +1,14 @@
-let audioIn;
+let mic;
 
 function setup() {
     createCanvas(200, 200);
-    audioIn = new p5.AudioIn();
-    audioIn.getSources(gotSources);
-    audioIn.start();
-}
-
-function gotSources(deviceList) {
-    if (deviceList.length > 0) {
-        //set the source to the first item in the deviceList array
-        audioIn.setSource(3);
-        let currentSource = deviceList[audioIn.currentSource];
-        text('set source to: ' + currentSource.deviceId, 5, 20, width);
-    }
+    mic = new p5.AudioIn();
+    mic.start();
 }
 
 function draw() {
     background(0);
-    let vol = audioIn.getLevel();
+    let vol = mic.getLevel();
     ellipse(100, 100, vol * 200, vol * 200);
     console.log(vol);
 }
