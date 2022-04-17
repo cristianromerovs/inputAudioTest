1543004b97035a9c4b781973e2380a0490dcb16b
Cristian Eduardo Romero Valdes <cristianromerovs@gmail.com>
2022-04-17 19:38:52 -0400

    fix 2

--- sketch,.js
+++ sketch,.js
@@ -16,30 +16,11 @@ function gotSources(deviceList) {
   }
 }
 
-let colorChange = 0;
-setInterval(function () {
-  colorChange++;
-  console.log(colorChange);
-  if (colorChange > 3) {
-    colorChange = 1;
-  }
-}, 5000);
-
 function draw() {
-  let vol = audioIn.getLevel();
-  if (colorChange === 1) {
-    backgroundColor.setRed(vol * 1000);
-    backgroundColor.setGreen(0);
-    backgroundColor.setBlue(0);
-  } else if (colorChange === 2) {
-    backgroundColor.setGreen(vol * 1000);
-    backgroundColor.setRed(0);
-    backgroundColor.setBlue(0);
-  } else if (colorChange === 3) {
-    backgroundColor.setBlue(vol * 1000);
-    backgroundColor.setRed(0);
-    backgroundColor.setGreen(0);
-  }
+  let vol = (audioIn.getLevel() * 1000).toFixed();
+  console.clear();
+  console.log(vol);
+  backgroundColor.setRed(vol);
   background(backgroundColor);
   //ellipse(100, 100, vol * 200, vol * 200);
   //rect(300, 200, 300, 200);
