5013e1ba476d65964b8fa130138cbaefcd6e0ccb
Cristian Eduardo Romero Valdes <cristianromerovs@gmail.com>
2022-04-17 16:50:40 -0400

    test

--- sketch,.js
+++ sketch,.js
@@ -1,14 +1,31 @@
-let mic;
+// let mic;
 
-function setup() {
-    createCanvas(200, 200);
-    mic = new p5.AudioIn();
-    mic.start();
+// function setup() {
+//     createCanvas(200, 200);
+//     mic = new p5.AudioIn();
+//     mic.start();
+// }
+
+// function draw() {
+//     background(0);
+//     let vol = mic.getLevel();
+//     ellipse(100, 100, vol * 200, vol * 200);
+//     console.log(vol);
+// }
+
+let audioIn;
+
+function setup(){
+  text('getting sources...', 0, 20);
+  audioIn = new p5.AudioIn();
+  audioIn.getSources(gotSources);
 }
 
-function draw() {
-    background(0);
-    let vol = mic.getLevel();
-    ellipse(100, 100, vol * 200, vol * 200);
-    console.log(vol);
+function gotSources(deviceList) {
+  if (deviceList.length > 0) {
+    //set the source to the first item in the deviceList array
+    audioIn.setSource(1);
+    let currentSource = deviceList[audioIn.currentSource];
+    text('set source to: ' + currentSource.deviceId, 5, 20, width);
+  }
 }
