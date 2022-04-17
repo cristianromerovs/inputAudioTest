db7b3146e8b02a66891c65edf133ec1aa30f308e
Cristian Eduardo Romero Valdes <cristianromerovs@gmail.com>
2022-04-17 19:27:08 -0400

    test

--- sketch,.js
+++ sketch,.js
@@ -16,9 +16,30 @@ function gotSources(deviceList) {
   }
 }
 
+let colorChange = 0;
+setInterval(function () {
+  colorChange++;
+  console.log(colorChange);
+  if (colorChange > 3) {
+    colorChange = 1;
+  }
+}, 5000);
+
 function draw() {
   let vol = audioIn.getLevel();
-  backgroundColor.setRed(vol * 400);
+  if (colorChange === 1) {
+    backgroundColor.setRed(vol * 1000);
+    backgroundColor.setGreen(0);
+    backgroundColor.setBlue(0);
+  } else if (colorChange === 2) {
+    backgroundColor.setGreen(vol * 1000);
+    backgroundColor.setRed(0);
+    backgroundColor.setBlue(0);
+  } else if (colorChange === 3) {
+    backgroundColor.setBlue(vol * 1000);
+    backgroundColor.setRed(0);
+    backgroundColor.setGreen(0);
+  }
   background(backgroundColor);
   //ellipse(100, 100, vol * 200, vol * 200);
   //rect(300, 200, 300, 200);
